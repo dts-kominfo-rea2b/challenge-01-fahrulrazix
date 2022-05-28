78d2d5d30b727718b0100b4d2272f92aa75658ed
fahrul razi <87560562+fahrulrazix@users.noreply.github.com>
2022-05-28 23:15:20 +0700

    Update index.js - Challenge 05
    Menambahkan attribut firstuser dan second user menggunakan array, set, map, dan object

--- index.js
+++ index.js
@@ -1,11 +1,80 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+let educationFirstUser = new Map([
+  ["nameSd", "SD 01"],
+  ["citySd", "Jakarta"],
+  ["graduateSd", 2016],
+  ["nameSmp", "SMP 02"],
+  ["citySmp", "Jakarta"],
+  ["graduateSmp", 2019],
+  ["nameSma", "SMA 03"],
+  ["citySma", "Tangerang"],
+  ["graduateSma", ""],
+]);
+
+let educationSecondUser = new Map([
+  ["nameSd", "SD 02"],
+  ["citySd", "Jakarta"],
+  ["graduateSd", 2010],
+  ["nameSmp", "SMP 03"],
+  ["citySmp", "Bogor"],
+  ["graduateSmp", 2013],
+  ["nameSma", "SMA 01"],
+  ["citySma", "Surabaya"],
+  ["graduateSma", 2016],
+  ["universitas", "Maju"],
+  ["universitasCity", "Tangerang"],
+  ["universitasGraduate", ""],
+]);
+
+let favoriteRestaurantFirstUsers = new Set();
+favoriteRestaurantFirstUsers.add("Bento");
+favoriteRestaurantFirstUsers.add("Sushi");
+favoriteRestaurantFirstUsers.add("Pancake");
+favoriteRestaurantFirstUsers.add("Eggy");
+favoriteRestaurantFirstUsers.add("Tempura");
+favoriteRestaurantFirstUsers.add("Bento");
+favoriteRestaurantFirstUsers.add("Eggy");
+favoriteRestaurantFirstUsers.add("Padang");
+favoriteRestaurantFirstUsers.add("Tteok");
+favoriteRestaurantFirstUsers.add("Sushi");
+favoriteRestaurantFirstUsers.add("Sushi");
+
+let favoriteRestaurantSecondUsers = new Set();
+favoriteRestaurantSecondUsers.add("Tempura");
+favoriteRestaurantSecondUsers.add("Bento");
+favoriteRestaurantSecondUsers.add("Sushi");
+favoriteRestaurantSecondUsers.add("Pancake");
+favoriteRestaurantSecondUsers.add("Padang");
+favoriteRestaurantSecondUsers.add("Katsu");
+favoriteRestaurantSecondUsers.add("Geprek");
+favoriteRestaurantSecondUsers.add("Pancake");
+favoriteRestaurantSecondUsers.add("Eggy");
+
+const firstUser = {
+  name: "Monica",
+  gender: "Female",
+  age: 17,
+  email: "monica@dingdong.com",
+  favoritColor: ["Yellow", "Pink", "White", "Purple"],
+  isHavePet: true,
+  educationFirstUser,
+  favoriteRestaurantFirstUsers,
+};
+const secondUser = {
+  name: "Wendy",
+  gender: "Male",
+  age: 23,
+  email: "wendy@dingdong.com",
+  favoritColor: ["Blue", "Black", "Grey"],
+  isHavePet: false,
+  educationSecondUser,
+  favoriteRestaurantSecondUsers,
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
